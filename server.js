46d81c2556a1f0dcebc0232fac3499ca0322afb6
RzWilliam <williamrodriguez0621@gmail.com>
2024-10-17 23:09:54 +0200

    🔧 Update server

--- server.js
+++ server.js
@@ -1,44 +1,40 @@
-const express = require("express");
-const http = require("http");
-const socketIo = require("socket.io");
-const next = require("next");
+import express from 'express';
+import http from 'http';
+import ip from 'ip';
+import { Server } from 'socket.io';
+import cors from 'cors';
 
-const port = 3000;
-const dev = process.env.NODE_ENV !== "production";
-const app = next({ dev });
-const handle = app.getRequestHandler();
-
-app.prepare().then(() => {
-  const expressApp = express();
-  const server = http.createServer(expressApp);
-  const io = socketIo(server, {
+const app = express();
+const server = http.createServer(app);
+const PORT = process.env.PORT || 3000; // Utilisation de la variable d'environnement PORT ou 3000 par défaut
+const io = new Server(server, {
     cors: {
-      origin: "*",
-    },
+        origin: '*',
+    }
+});
+
+app.use(cors());
+
+app.get('/', (req, res) => {
+  res.json('ip address: http://' + ip.address() + ':' + PORT);    
+});
+
+io.on("connection", (socket) => {
+  console.log("Un utilisateur est connecté");
+
+  socket.on("message", (data) => {
+    const { username, message } = data;
+    console.log(`${username} a envoyé un message : ${message}`);
+
+    io.emit("message", { username, message });
   });
 
-  io.on("connection", (socket) => {
-    console.log("Un utilisateur est connecté");
-
-    socket.on("message", (data) => {
-      const { username, message } = data;
-      console.log(`${username} a envoyé un message : ${message}`);
-
-      // Diffuse le message à tous les clients, avec le pseudo
-      io.emit("message", { username, message });
-    });
-
-    socket.on("disconnect", () => {
-      console.log("Utilisateur déconnecté");
-    });
+  socket.on("disconnect", () => {
+    console.log("Utilisateur déconnecté");
   });
+});
 
-  expressApp.all("*", (req, res) => {
-    return handle(req, res);
-  });
-
-  server.listen(port, (err) => {
-    if (err) throw err;
-    console.log(`Ready on http://localhost:${port}`);
-  });
+server.listen(PORT, (err) => {
+  if (err) throw err;
+  console.log(`Ready on http://localhost:${PORT}`);
 });
